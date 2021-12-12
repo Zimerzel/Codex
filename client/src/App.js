7b1c52f9a5a6d7fb929dd50abdb54b814a57b235
Imerzel <zach.imerzel@gmail.com>
2021-12-12 13:23:47 -0600

    added base app routing, downgraded react-app-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Games from './components/Games';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Header />
+        
+        <Switch>
+          <Route exact path='/' render= {() => (<Redirect to='/home'/>)} />
+          <Route exact path='/home' component={Home} />
+            <Suspense fallback={<div id="fallback">Loading...</div>}>
+              <Route exact path='/games' component={Games} />
+            </Suspense>
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+
   );
 }
 
---
+++ client/src/components/Games.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Games(){
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Games;
