94108ae6f11f4c6738434ac1a42985f7bcf9c766
Eric Webber <ewebber283@gmail.com>
2021-12-15 19:56:14 -0600

    set up initial post route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password: 'password',
+    database: 'codex_db'
+})
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/api/insert', (req, res) => {
+
+    const username = req.body.username;
+    const review = req.body.review;
+
+    const sqlInsert = "INSERT INTO comments (username, review) VALUES (?,?)"
+    db.query(sqlInsert, (username, review), (err, result) => {
+
+    });
+});
 
 app.listen(3001, () => {
     console.log('running on port 3001')
