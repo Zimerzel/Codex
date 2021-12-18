3c9382ff9f98cab7f3b0eab850964a8b62bc827b
Eric Webber <ewebber283@gmail.com>
2021-12-18 16:33:24 -0600

    set up typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,26 @@
+const { User, Review } = require('../models');
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
-    }
+    reviews: async(parent, { username }) => {
+      const params = username ? { username } : {};
+      return Review.find(params).sort({ createdAt: -1 });
+    },
+    review: async (parents, { _id }) => {
+      return Review.findOne({ _id });
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('reviews');
+    },
+    user: async(parents, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('reviews');
+    },
   }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,36 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql `
+
+    type Review {
+        _id: ID
+        reviewText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        reviews: [Review]
+        friends: [User]
+    }
     type Query {
-        helloWorld: String
+        users: [User]
+        user(username: String!): User
+        reviews(username: String): [Review]
+        review(_id: ID!): Review
     }
 `;
 
