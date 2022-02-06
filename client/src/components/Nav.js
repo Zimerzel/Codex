717fbd6b5858af5f62919c9142d007b8d94ee18e
Zimerzel <zach.imerzel@gmail.com>
2022-02-06 09:30:08 -0600

    removed video component because its not supported by api, made game list a gridcd

---
+++ client/src/components/Nav.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+
+const Nav = () => {
+  //when user clicks a menu link or the element lost focus within the nav menu, close it
+  
+  function navDrop() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classNameList.contains('show')) {
+          openDropdown.classNameList.remove('show');
+        }
+      }
+    }
+  }
+
+  return (
+    <>
+    <nav className="main__nav">
+      <ul>
+      <li>
+          <HashLink to="#games" smooth className="nav-link hover-underline-animation" id="games-btn">
+            Games
+          </HashLink>
+        </li>
+        <li>
+          <NavLink to="/" className="nav-link hover-underline-animation">
+            Home
+          </NavLink>
+        </li>
+      </ul>
+      <nav className='nav__dropdown'>
+        <div className="dropdown">
+          <button onClick={navDrop} className="dropbtn"></button>
+          <div id="myDropdown" className="dropdown-content">
+            <ul>
+              <li>
+                <NavLink to="/" className="nav-link" onClick={navDrop}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <HashLink to="#games" smooth className="nav-link" id="games-btn">
+                  Games
+                </HashLink>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </nav>
+    </>
+  );
+};
+
+export default Nav;
